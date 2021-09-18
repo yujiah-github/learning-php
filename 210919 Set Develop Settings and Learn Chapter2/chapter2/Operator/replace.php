d90b6efba1e0e826b37cfd2b738a83b9ff24179b
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-19 03:07:03 +0900

    210919 Finish Chapter2 Sourcefiles

---
+++ 210919 Set Develop Settings and Learn Chapter2/chapter2/Operator/replace.php
@@ -0,0 +1,23 @@
+<?php
+$a = 5;
+echo $a."<br>";
+
+$a += 3;
+echo $a."<br>";
+
+$a -= 4;
+echo $a."<br>";
+
+$a *= 2;
+echo $a."<br>";
+
+$a /= 4;
+echo $a."<br>";
+
+$a %= 2;
+echo $a."<br>";
+
+$a = "딸기"
+$a .="주스";
+echo $a."<br>";
+?>
