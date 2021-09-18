bf0fa76d328ce4fdb7a93540db3941351d8ec797
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-19 02:29:26 +0900

    210919 Add Part of Chapter2 Sourcefiles

---
+++ 210919 Set Develop Settings and Learn Chapter2/chapter2/Basic Data Type/string.php
@@ -0,0 +1,16 @@
+<?php
+$title = "<h3>연락처</h3>";
+$name = "홍길동";
+$phone = "010-1234-5668";
+$address = "경기도 성남시 분당구";
+$email = "user@codingschool.biz";
+
+echo "<br>";
+
+echo $title;
+echo "이름 : $name<br>"; 
+echo "주소 : $address<br>"; 
+echo "전화번호 : $phone<br>"; 
+echo "이메일 : $email<br>"; 
+
+?>
