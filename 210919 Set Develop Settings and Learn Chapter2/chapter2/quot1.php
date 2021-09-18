5e2077cf2a9c41c0ad3f57d7712188be515df664
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-19 01:58:20 +0900

    210919 Add part of Chapter2 Sourcefiles

---
+++ 210919 Set Develop Settings and Learn Chapter2/chapter2/quot1.php
@@ -0,0 +1,11 @@
+<?php
+$name = "유지아";
+echo $name;
+echo "님 반갑습니다.";
+echo "<br>";
+
+$name = "유지아";
+echo $name;
+echo '님 반갑습니다.';
+
+?>
---
+++ 210919 Set Develop Settings and Learn Chapter2/chapter2/quot2.php
@@ -0,0 +1,8 @@
+<?php
+$name = "유지아";
+echo "$name 님 반갑습니다.";
+echo "<br>";
+
+$name = "유지아";
+echo '$name 님 반갑습니다.';
+?>
