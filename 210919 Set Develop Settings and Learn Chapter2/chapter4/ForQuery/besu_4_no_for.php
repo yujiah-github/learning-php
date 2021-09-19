dd0cdc2853766cc462db90d8d00a9c9a238945ac
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-20 02:47:44 +0900

    210920 Add Part of Chapter 4 Sourcecode files

---
+++ 210919 Set Develop Settings and Learn Chapter2/chapter4/ForQuery/besu_4_no_for.php
@@ -0,0 +1,20 @@
+<?php
+
+$count = 0;
+$ok = true;
+for($i = 500; $ <= 700; $i++)
+{
+	if ($i % 4 != 0)
+	{
+		echo $i." ";
+		$count++;
+		$ok = true;
+	}
+	if($count != 0 && $count % 10 == 0 && $ok == true)
+	{
+		echo "<br>";
+		$ok = false;
+	}
+}
+
+?>
