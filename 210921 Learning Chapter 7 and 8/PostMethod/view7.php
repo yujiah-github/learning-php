6a7565089c5c97e11b499020be057d041238aa4c
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-22 02:41:09 +0900

    210922 Add Part of Chatper 7 'hidden' sourcecode files

---
+++ 210921 Learning Chapter 7 and 8/PostMethod/view7.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8">
+<link href="stylesheet" href="style.css">
+</head>
+<?php
+	$userid = $_POST['userid'];
+	?>
+<body>
+<ul>
+	<li>관리자 아이디 : <? =$userid ?></li>
+</ul>
+</body>
+
+</html>	
