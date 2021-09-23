a305cb25ce0cbe26b069beba49f481935aad4782
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-23 17:57:53 +0900

    210923 Cookie Use

---
+++ 210923 Learning Chapter 8/chapter8/Cookie/cookie_use.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_COOKIE['userid']) && isset($_COOKIE['username']))
+{
+	$userid = $_COOKIE['userid'];
+	$username = $_COOKIE['username'];
+
+	echo "userid 쿠키 : " . $userid. "<br>";
+	echo "username 쿠키 : ". $username. "<Br>";
+}
+
+else
+{
+	echo "쿠키가 생성되지 않았다!";
+}
+?>
