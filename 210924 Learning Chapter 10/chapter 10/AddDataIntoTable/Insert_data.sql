f45ce015e6b220be38dd260ab8f8a58a5dff2563
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-24 10:17:31 +0900

    210924 Add Markdown file

--- 210924 Learning Chapter 10/chapter 10/AddDataIntoTable/Insert_data.sql
+++ 210924 Learning Chapter 10/chapter 10/AddDataIntoTable/Insert_data.sql
@@ -3,4 +3,3 @@
 > insert into friend(num, tel, address) values ('송예진', '031-123-4567');
 ![](https://images.velog.io/images/cil05265/post/d4243dc3-316d-41f7-ad3a-542943ceba49/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA%202021-09-24%20%E1%84%8B%E1%85%A9%E1%84%8C%E1%85%A5%E1%86%AB%2010.01.51.png)
 
-
